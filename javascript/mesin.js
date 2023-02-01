f612552b7c8390093a912acf8ca2447ca00d8bcb
gunawan-ex <adektersayang88@gmail.com>
2023-02-01 16:02:02 +0800

    p

---
+++ javascript/mesin.js
@@ -0,0 +1,27 @@
+function add() {
+    var num1 = document.getElementById("num1").value;
+    var num2 = document.getElementById("num2").value;
+    var result = parseInt(num1) + parseInt(num2);
+    document.getElementById("result").innerHTML = result;
+  }
+
+  function subtract() {
+    var num1 = document.getElementById("num1").value;
+    var num2 = document.getElementById("num2").value;
+    var result = parseInt(num1) - parseInt(num2);
+    document.getElementById("result").innerHTML = result;
+  }
+
+  function multiply() {
+    var num1 = document.getElementById("num1").value;
+    var num2 = document.getElementById("num2").value;
+    var result = parseInt(num1) * parseInt(num2);
+    document.getElementById("result").innerHTML = result;
+  }
+
+  function divide() {
+    var num1 = document.getElementById("num1").value;
+    var num2 = document.getElementById("num2").value;
+    var result = parseInt(num1) / parseInt(num2);
+    document.getElementById("result").innerHTML = result;
+  }
